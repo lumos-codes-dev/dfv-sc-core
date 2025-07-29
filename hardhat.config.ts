5520e0be792c24314245cd1130b7b3aedc4fce47
shhmeks <paranchich2002@gmail.com>
2025-07-29 13:10:43 +0300

    chore: initialize project
    - Added package.json to manage project dependencies and scripts.
    - Included devDependencies for Hardhat and Hardhat Toolbox.
    - Created tsconfig.json for TypeScript configuration with strict settings.

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEYS = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY.split(",") : [];
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    hardhat: {},
+    ethereum: {
+      url: "https://ethereum-rpc.publicnode.com",
+      accounts: PRIVATE_KEYS,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEYS,
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      ethereum: ETHERSCAN_API_KEY,
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+};
+
+export default config;
