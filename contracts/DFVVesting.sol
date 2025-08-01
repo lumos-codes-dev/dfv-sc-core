1e48d533c677d3d16b4d962bfb696b9031e859f2
shhmeks <paranchich2002@gmail.com>
2025-08-01 19:10:51 +0300

    feat: enhance DFVVesting and IDFVVesting with category management and custom pool creation

--- contracts/DFVVesting.sol
+++ contracts/DFVVesting.sol
@@ -18,12 +18,21 @@ contract DFVVesting is IDFVVesting, Ownable {
     /// @dev Used for percentage calculations, e.g., 10000 basis points = 100%
     uint256 public constant BASIS_POINTS_DENOMINATOR = 10000;
 
+    /// @notice Constant for maximum batch size
+    /// @dev Used to limit the number of beneficiaries in a single batch operation
+    uint256 public constant MAX_BATCH_SIZE = 100;
+
     /// @dev The token that is being vested
     IERC20 public token;
 
     /// @notice Amount of tokens which are reserved in vestings.
     /// @dev It is using for control allocation.
     uint256 public totalVested;
+
+    /// @notice Mapping of vesting categories to their rules.
+    /// @dev This mapping defines the rules for each vesting category,
+    /// including maximum amounts, maximum participants, and schedules.
+    mapping(VestingCategory category => CategoryRules rules) public categories;
 
     /// @notice Mapping of beneficiary addresses to their vesting pools
     mapping(address beneficiary => Pool[] vestingPools) public pools;
@@ -35,42 +44,102 @@ contract DFVVesting is IDFVVesting, Ownable {
         _;
     }
 
-    /// @notice Constructor to initialize the vesting contract with the token address
+    modifier onlyCorrectBatchSize(uint256 batchSize_) {
+        require(batchSize_ != 0, NoParamsProvided());
+        require(batchSize_ <= MAX_BATCH_SIZE, BatchSizeExceedsLimit());
+        _;
+    }
+
+    /// @notice Constructor to initialize the vesting contract with the token address and set up initial categories
     /// @param token_ The address of the token to be vested
     /// @dev It uses the Ownable constructor to set deployer as the owner
     constructor(address token_) Ownable(_msgSender()) onlyNonZeroAddress(token_) {
         token = IERC20(token_);
+
+        // ! Hardcoded mock data
+        categories[VestingCategory.BlindBelievers] = CategoryRules({
+            totalVestedAmountLeft: 20_826_000_000 * 1e18, // 15% of total supply
+            qty: 694_200_000 * 1e18,
+            beneficiariesLeft: 30,
+            // rewards will be unlocking each second during 1 year
+            schedule: Schedule({cliffDuration: 0, periodDuration: 1, periodCount: 12 * 30 days}),
+            initialUnlock: 0
+        });
+
+        categories[VestingCategory.EternalHODLers] = CategoryRules({
+            totalVestedAmountLeft: 13_884_000_000 * 1e18, // 10% of total supply
+            qty: 69_420_000 * 1e18,
+            beneficiariesLeft: 200,
+            schedule: Schedule({cliffDuration: 0, periodDuration: 30 days, periodCount: 12}),
+            initialUnlock: 0
+        });
+
+        categories[VestingCategory.DiamondHands] = CategoryRules({
+            totalVestedAmountLeft: 6_942_000_000 * 1e18, // 5% of total supply
+            qty: 6_942_000 * 1e18,
+            beneficiariesLeft: 1000,
+            schedule: Schedule({cliffDuration: 0, periodDuration: 30 days, periodCount: 12}),
+            initialUnlock: 0
+        });
+
+        categories[VestingCategory.JustHODLers] = CategoryRules({
+            totalVestedAmountLeft: 13_884_000_000 * 1e18, // 10% of total supply
+            qty: 694_200 * 1e18,
+            beneficiariesLeft: 20000,
+            schedule: Schedule({cliffDuration: 0, periodDuration: 30 days, periodCount: 12}),
+            initialUnlock: 0
+        });
+
+        categories[VestingCategory.CommunityAirdrop] = CategoryRules({
+            totalVestedAmountLeft: 13_884_000_000 * 1e18, // 10% of total supply
+            qty: 0, // ! Needs to be specified
+            beneficiariesLeft: 10000,
+            schedule: Schedule({cliffDuration: 0, periodDuration: 30 days, periodCount: 12}),
+            initialUnlock: 0
+        });
+    }
+
+    /// @notice Function to create a new vesting pool for a specific category
+    /// @param params_ The parameters for creating a custom vesting pool
+    /// @dev See `CreateCustomVestingPoolParams` for details on the parameters
+    function createCategoryPool(CreateCategoryPoolParams calldata params_) external onlyOwner {
+        _createCategoryPool(params_);
     }
 
     /// @notice Function to create a new vesting pool
-    /// @param beneficiary_ The address of the beneficiary who will receive the vested tokens
-    /// @param amount_ The total amount of tokens to be vested
-    /// @param schedule_ The vesting schedule defining start, cliff duration, and vesting
-    /// @param initialUnlock_ The percentage of tokens that can be claimed immediately after the cliff
-    function createVestingPool(
-        address beneficiary_,
-        uint256 amount_,
-        Schedule calldata schedule_,
-        uint256 initialUnlock_
-    ) external onlyOwner {
-        _createVestingPool(beneficiary_, amount_, schedule_, initialUnlock_);
+    /// @param params_ The parameters for creating a custom vesting pool
+    /// @dev See `CreateCustomVestingPoolParams` for details on the parameters
+    function createCustomVestingPool(CreateCustomVestingPoolParams calldata params_) external onlyOwner {
+        _createVestingPool(params_, false);
     }
 
     /// @notice Function to create vesting pools for multiple beneficiaries
-    /// @param beneficiaries_ An array of addresses of beneficiaries who will receive the vested tokens
-    /// @param amounts_ An array of total amounts of tokens to be vested for each beneficiary
-    /// @param schedule_ The vesting schedule defining start, cliff duration, and vesting duration
-    /// @param initialUnlock_ The percentage of tokens that can be claimed immediately after the cliff
-    function createVestingPoolBatch(
-        address[] calldata beneficiaries_,
-        uint256[] calldata amounts_,
-        Schedule calldata schedule_,
-        uint256 initialUnlock_
-    ) external onlyOwner {
-        require(beneficiaries_.length == amounts_.length, ArraysLengthMismatch());
+    /// @param params_ The parameters object array for creating custom vesting pools
+    /// @dev See `CreateCustomVestingPoolParams` for details on the parameters
+    function createCustomVestingPoolBatch(
+        CreateCustomVestingPoolParams[] calldata params_
+    ) external onlyOwner onlyCorrectBatchSize(params_.length) {
+        for (uint256 i; i < params_.length; ) {
+            _createVestingPool(params_[i], false);
 
-        for (uint256 i; i < beneficiaries_.length; i++) {
-            _createVestingPool(beneficiaries_[i], amounts_[i], schedule_, initialUnlock_);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @notice Function to create vesting pools for multiple beneficiaries in a specific category
+    /// @param params_ The parameters for creating a category pool
+    /// @dev See `CreateCategoryPoolParams` for details on the parameters
+    function createCategoryPoolBatch(
+        CreateCategoryPoolParams[] calldata params_
+    ) external onlyOwner onlyCorrectBatchSize(params_.length) {
+        for (uint256 i; i < params_.length; ) {
+            _createCategoryPool(params_[i]);
+
+            unchecked {
+                ++i;
+            }
         }
     }
 
@@ -115,28 +184,63 @@ contract DFVVesting is IDFVVesting, Ownable {
         }
     }
 
+    /// @notice Internal function to create a vesting pool for a specific category
+    /// @param params_ The parameters for creating a custom vesting pool
+    /// @dev It checks if the category has enough allocation left and if there are beneficiaries left
+    /// @dev It updates the category's allocation and beneficiaries count
+    function _createCategoryPool(CreateCategoryPoolParams calldata params_) internal {
+        CategoryRules storage category = categories[params_.category];
+
+        uint256 amount = params_.category != VestingCategory.CommunityAirdrop
+            ? category.qty * params_.multiplierOrAmount
+            : params_.multiplierOrAmount;
+
+        require(category.beneficiariesLeft != 0, CategoryBeneficiariesAllSet(params_.category));
+        require(category.totalVestedAmountLeft >= amount, NotEnoughAllocationLeft(params_.category));
+
+        category.beneficiariesLeft--;
+        category.totalVestedAmountLeft -= amount;
+
+        _createVestingPool(
+            CreateCustomVestingPoolParams({
+                beneficiary: params_.beneficiary,
+                amount: amount,
+                start: params_.start,
+                schedule: category.schedule,
+                initialUnlock: category.initialUnlock
+            }),
+            true
+        );
+    }
+
     /// @notice Internal function to create a vesting pool
-    /// @param beneficiary_ The address of the beneficiary who will receive the vested tokens
-    /// @param amount_ The total amount of tokens to be vested
-    /// @param schedule_ The vesting schedule defining start, cliff duration, and vesting duration
-    /// @param initialUnlock_ The percentage of tokens that can be claimed immediately after the cliff
+    /// @param params_ The parameters for creating a custom vesting pool
+    /// @dev See `CreateCustomVestingPoolParams` for details on the parameters
+    /// @dev Start timestamp is set to the current block timestamp if it is less or equal to the current time
     function _createVestingPool(
-        address beneficiary_,
-        uint256 amount_,
-        Schedule memory schedule_,
-        uint256 initialUnlock_
-    ) internal onlyNonZeroAddress(beneficiary_) {
-        require(amount_ != 0 && schedule_.vestingDuration != 0, ZeroAmount());
-        require(schedule_.start >= block.timestamp, StartMustBeInFuture());
-        require(initialUnlock_ <= BASIS_POINTS_DENOMINATOR, InitialUnlockExceedsLimit());
+        CreateCustomVestingPoolParams memory params_,
+        bool isCategory_
+    ) internal onlyNonZeroAddress(params_.beneficiary) {
+        require(
+            params_.amount != 0 && params_.schedule.periodDuration != 0 && params_.schedule.periodCount != 0,
+            ZeroAmount()
+        );
+        require(params_.initialUnlock <= BASIS_POINTS_DENOMINATOR, InitialUnlockExceedsLimit());
 
-        Pool memory pool = Pool({amount: amount_, schedule: schedule_, initialUnlock: initialUnlock_, claimed: 0});
-        pools[beneficiary_].push(pool);
-        totalVested += amount_;
+        Pool memory pool = Pool({
+            amount: params_.amount,
+            start: params_.start > block.timestamp ? params_.start : block.timestamp,
+            schedule: params_.schedule,
+            initialUnlock: params_.initialUnlock,
+            claimed: 0,
+            isCategory: isCategory_
+        });
+        pools[params_.beneficiary].push(pool);
+        totalVested += params_.amount;
 
-        token.safeTransferFrom(_msgSender(), address(this), amount_);
+        token.safeTransferFrom(_msgSender(), address(this), params_.amount);
 
-        emit VestingPoolCreated(beneficiary_, pool);
+        emit VestingPoolCreated(params_.beneficiary, pool);
     }
 
     /// @notice Internal function to claim vested tokens for a specific beneficiary
@@ -172,18 +276,20 @@ contract DFVVesting is IDFVVesting, Ownable {
     /// @param pool_ The vesting pool for which to calculate the unlocked amount
     /// @return The amount of tokens that are unlocked and can be claimed
     function _calculateUnlockedAmount(Pool memory pool_) internal view returns (uint256) {
-        uint256 startTimestamp = pool_.schedule.start + pool_.schedule.cliffDuration;
+        Schedule memory schedule = pool_.schedule;
+        uint256 cliffEndTimestamp = pool_.start + schedule.cliffDuration;
         uint256 currentTimestamp = block.timestamp;
-        if (currentTimestamp < startTimestamp) return 0;
+
+        if (currentTimestamp < cliffEndTimestamp) return 0;
 
         uint256 totalAmount = pool_.amount;
         uint256 initialAmount = (totalAmount * pool_.initialUnlock) / BASIS_POINTS_DENOMINATOR;
 
+        uint256 passedPeriods = (currentTimestamp - pool_.start - schedule.cliffDuration) / schedule.periodDuration;
+
         return
-            currentTimestamp >= startTimestamp + pool_.schedule.vestingDuration
+            passedPeriods >= schedule.periodCount
                 ? totalAmount
-                : ((totalAmount - initialAmount) * (currentTimestamp - startTimestamp)) /
-                    pool_.schedule.vestingDuration +
-                    initialAmount;
+                : (((totalAmount - initialAmount) * (passedPeriods)) / schedule.periodCount) + initialAmount;
     }
 }
--- contracts/interfaces/IDFVVesting.sol
+++ contracts/interfaces/IDFVVesting.sol
@@ -9,25 +9,78 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  */
 interface IDFVVesting {
     /// @notice Structure to define a vesting schedule
-    /// @param start The timestamp when the vesting timeline starts.
     /// @param cliffDuration The duration of the cliff period in seconds.
-    /// @param vestingDuration The total duration of the vesting period in seconds.
+    /// @param periodDuration The duration of each vesting period in seconds.
+    /// @param periodCount The number of vesting periods.
     struct Schedule {
-        uint256 start;
         uint256 cliffDuration;
-        uint256 vestingDuration;
+        uint256 periodDuration;
+        uint256 periodCount;
     }
 
     /// @notice Structure which describe vesting pool
     /// @param amount The total amount of tokens to be vested.
+    /// @param start The timestamp when the vesting timeline starts.
     /// @param schedule The vesting schedule.
     /// @param initialUnlock The percentage of tokens that can be claimed immediately after the cliff.
     /// @param claimed The amount of tokens that have already been claimed.
     struct Pool {
         uint256 amount;
+        uint256 start;
         Schedule schedule;
         uint256 initialUnlock;
         uint256 claimed;
+        bool isCategory;
+    }
+
+    /// @notice Enum to categorize vesting pools
+    /// @dev This enum is used to categorize different types of vesting pools.
+    enum VestingCategory {
+        BlindBelievers,
+        EternalHODLers,
+        DiamondHands,
+        JustHODLers,
+        CommunityAirdrop
+    }
+
+    /// @notice Structure to define rules for a vesting category
+    /// @param totalVestedAmountLeft The total amount of tokens left to be vested for this category.
+    /// @param beneficiariesLeft The number of beneficiaries left for this category.
+    /// @param schedule The vesting schedule defining cliff duration and vesting periods.
+    /// @param initialUnlock The percentage of tokens that can be claimed immediately after the cliff.
+    struct CategoryRules {
+        uint256 totalVestedAmountLeft;
+        uint256 qty;
+        uint256 beneficiariesLeft;
+        Schedule schedule;
+        uint256 initialUnlock;
+    }
+
+    /// @notice Structure to define parameters for creating a custom vesting pool
+    /// @param beneficiary The address of the beneficiary who will receive the vested tokens.
+    /// @param amount The total amount of tokens to be vested for the beneficiary.
+    /// @param start The timestamp when the vesting timeline starts for the beneficiary.
+    /// @param schedule The vesting schedule defining cliff duration and vesting periods.
+    /// @param initialUnlock The percentage of tokens that can be claimed immediately after the cliff (in basis points).
+    struct CreateCustomVestingPoolParams {
+        address beneficiary;
+        uint256 amount;
+        uint256 start;
+        Schedule schedule;
+        uint256 initialUnlock;
+    }
+
+    /// @notice Structure to define parameters for creating a category pool
+    /// @param category The vesting category for which the pool is being created.
+    /// @param beneficiary The address of the beneficiary who will receive the vested tokens.
+    /// @param multiplierOrAmount The multiplier for BlindBelievers (0), EternalHODLers (1), DiamondHands
+    /// (2), JustHODLers (3) categories or the amount of tokens to be vested for CommunityAirdrop (4) category.
+    /// @param start The timestamp when the vesting timeline starts for the beneficiary in this category.
+    struct CreateCategoryPoolParams {
+        VestingCategory category;
+        address beneficiary;
+        uint256 multiplierOrAmount;
+        uint256 start;
     }
 
     /// @notice Event emitted when a new vesting pool is created
@@ -51,11 +104,11 @@ interface IDFVVesting {
     /// @dev Error thrown when a zero amount is provided or calculated
     error ZeroAmount();
 
-    /// @dev Error thrown when the start time of a vesting schedule is in the past
-    error StartMustBeInFuture();
+    /// @dev Error thrown when no parameters are provided for a function that requires them
+    error NoParamsProvided();
 
-    /// @dev Error thrown when the length of arrays do not match
-    error ArraysLengthMismatch();
+    /// @dev Error thrown when the batch size exceeds the maximum allowed size
+    error BatchSizeExceedsLimit();
 
     /// @dev Error thrown when no allocations are found for a beneficiary during a claim
     error NoAllocationsFound();
@@ -63,29 +116,23 @@ interface IDFVVesting {
     /// @dev Error thrown when the initial unlock percentage exceeds the limit 100% (10000 basis points)
     error InitialUnlockExceedsLimit();
 
+    /// @dev Error thrown when the total vested amount left for a category is less than the requested amount
+    /// @param category The vesting category for which the allocation is insufficient
+    error NotEnoughAllocationLeft(VestingCategory category);
+
+    /// @dev Error thrown when the number of beneficiaries left for a category is zero
+    /// @param category The vesting category for which there are no beneficiaries left
+    error CategoryBeneficiariesAllSet(VestingCategory category);
+
     /// @notice Function to create a new vesting pool
-    /// @param beneficiary_ The address of the beneficiary who will receive the vested tokens
-    /// @param amount_ The total amount of tokens to be vested
-    /// @param schedule_ The vesting schedule defining start, cliff duration, and vesting
-    /// @param initialUnlock_ The percentage of tokens that can be claimed immediately after the cliff
-    function createVestingPool(
-        address beneficiary_,
-        uint256 amount_,
-        Schedule calldata schedule_,
-        uint256 initialUnlock_
-    ) external;
+    /// @param params_ The parameters for creating a custom vesting pool
+    /// @dev See `CreateCustomVestingPoolParams` for details on the parameters
+    function createCustomVestingPool(CreateCustomVestingPoolParams calldata params_) external;
 
     /// @notice Function to create vesting pools for multiple beneficiaries
-    /// @param beneficiaries_ An array of addresses of beneficiaries who will receive the vested tokens
-    /// @param amounts_ An array of total amounts of tokens to be vested for each beneficiary
-    /// @param schedule_ The vesting schedule defining start, cliff duration, and vesting duration
-    /// @param initialUnlock_ The percentage of tokens that can be claimed immediately after the cliff
-    function createVestingPoolBatch(
-        address[] calldata beneficiaries_,
-        uint256[] calldata amounts_,
-        Schedule calldata schedule_,
-        uint256 initialUnlock_
-    ) external;
+    /// @param params_ The parameters object array for creating custom vesting pools
+    /// @dev See `CreateCustomVestingPoolParams` for details on the parameters
+    function createCustomVestingPoolBatch(CreateCustomVestingPoolParams[] calldata params_) external;
 
     /// @notice Function to claim vested tokens for the caller
     /// @dev It claims amount of tokens from all vesting pools of the caller
