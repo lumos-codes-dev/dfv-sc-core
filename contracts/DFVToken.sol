86bde96d012a1af03eecef3841420c002dc508ff
shhmeks <paranchich2002@gmail.com>
2025-08-06 19:14:13 +0300

    feat: refactor DFVToken contract to include governance and voting capabilities; remove GovernanceToken contract

--- contracts/DFVToken.sol
+++ contracts/DFVToken.sol
@@ -2,20 +2,45 @@
 pragma solidity 0.8.28;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 /**
  * @title DFVToken
- * @notice ERC20 token representing DFV
- * @dev This contract inherits from OpenZeppelin's ERC20 implementation.
+ * @notice This contract implements a governance token with voting capabilities and permit functionality.
+ * @dev It extends OpenZeppelin's ERC20, ERC20Permit, and ERC20Votes contracts.
  */
-contract DFVToken is ERC20 {
+contract DFVToken is ERC20, ERC20Permit, ERC20Votes {
     /**
      * @notice Constructor to initialize the DFVToken contract
      * @dev Sets the token name and symbol and mints an initial supply
+     * @param vesting_ The address of the vesting contract which will hold the initial supply
      * @dev The initial supply is set to 138,840,000,000 tokens with 18 decimals
      * @dev The initial supply is minted to the deployer's address
      */
-    constructor() ERC20("DFV Token", "DFV") {
-        _mint(_msgSender(), 138_840_000_000 * 10 ** decimals());
+    constructor(address vesting_) ERC20("DFV Token", "DFV") ERC20Permit("DFV Token") {
+        _mint(vesting_, 138_840_000_000 * 10 ** decimals());
+    }
+
+    /**
+     * @notice Function to get the current nonce for a given owner
+     * @param owner_ The address of the owner whose nonce is being queried
+     * @return The current nonce for the owner
+     * @dev This function overrides the nonces function from ERC20Permit and Nonces
+     */
+    function nonces(address owner_) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner_);
+    }
+
+    /**
+     * @notice Function override the _update function to ensure compatibility with ERC20Votes
+     * @param from_ The address from which tokens are being transferred
+     * @param to_ The address to which tokens are being transferred
+     * @param value_ The amount of tokens being transferred
+     * @dev This function is required to ensure that the voting power is updated correctly
+     */
+    function _update(address from_, address to_, uint256 value_) internal override(ERC20, ERC20Votes) {
+        super._update(from_, to_, value_);
     }
 }
